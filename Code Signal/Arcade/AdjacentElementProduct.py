62ac72b9edf0c85041ef1682fcbf7f8f06c167f0
Anjana Kummari <anjana.rajasekhar1228@gmail.com>
2019-10-29 13:55:30 -0500

    Update AdjacentElementProduct.py

--- Code Signal/Arcade/AdjacentElementProduct.py
+++ Code Signal/Arcade/AdjacentElementProduct.py
@@ -1,7 +1,7 @@
 '''
 Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
-Example
+Example : 
 
 For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 adjacentElementsProduct(inputArray) = 21.
